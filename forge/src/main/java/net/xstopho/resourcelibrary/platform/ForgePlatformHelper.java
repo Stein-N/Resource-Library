49f86040ad5ca6dc5e2431ef3b65c1dfdc5f688c
Stein-N <nico-stein-98@outlook.de>
2024-04-27 23:45:22 +0200

    adding forge and backport to 1.20.1

---
+++ forge/src/main/java/net/xstopho/resourcelibrary/platform/ForgePlatformHelper.java
@@ -0,0 +1,9 @@
+package net.xstopho.resourcelibrary.platform;
+
+public class ForgePlatformHelper implements IPlatformHelper {
+
+    @Override
+    public Platforms getPlatform() {
+        return Platforms.FORGE;
+    }
+}
