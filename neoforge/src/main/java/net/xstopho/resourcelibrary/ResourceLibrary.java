2e2b91e1be00bd3fa503d8dc4a003f0ec4129258
Stein-N <nico-stein-98@outlook.de>
2024-04-28 00:21:35 +0200

    adding NeoForge

---
+++ neoforge/src/main/java/net/xstopho/resourcelibrary/ResourceLibrary.java
@@ -0,0 +1,23 @@
+package net.xstopho.resourcelibrary;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.xstopho.resourcelibrary.modifier.LootTableModifier;
+import net.xstopho.resourcelibrary.modifier.loot_tables.ChestLootTables;
+import net.xstopho.resourcelibrary.registration.RegistryObject;
+import net.xstopho.resourcelibrary.registration.RegistryProvider;
+
+@Mod(LibConstants.MOD_ID)
+public class ResourceLibrary {
+
+    final RegistryProvider<Item> ITEMS = RegistryProvider.get(Registries.ITEM, LibConstants.MOD_ID);
+    final RegistryObject<Item> TEST = ITEMS.register("test_item", () -> new Item(new Item.Properties()));
+
+    public ResourceLibrary(IEventBus eventBus) {
+        LootTableModifier modifier = LootTableModifier.get();
+
+        modifier.addToPool(TEST, 1f, 1f, ChestLootTables.SPAWN_BONUS_CHEST);
+    }
+}
---
+++ neoforge/src/main/java/net/xstopho/resourcelibrary/platform/NeoForgePlatformHelper.java
@@ -0,0 +1,8 @@
+package net.xstopho.resourcelibrary.platform;
+
+public class NeoForgePlatformHelper implements IPlatformHelper {
+    @Override
+    public Platforms getPlatform() {
+        return Platforms.NEOFORGE;
+    }
+}
