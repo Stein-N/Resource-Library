5b63e6314843c0f441fc4b86e4d65d97bbd20e9d
Stein-N <nico-stein-98@outlook.de>
2024-05-01 19:00:03 +0200

    deleting forge

--- forge/src/main/java/net/xstopho/resourcelibrary/platform/ForgePlatformHelper.java
+++
@@ -1,9 +0,0 @@
-package net.xstopho.resourcelibrary.platform;
-
-public class ForgePlatformHelper implements IPlatformHelper {
-
-    @Override
-    public Platforms getPlatform() {
-        return Platforms.FORGE;
-    }
-}
