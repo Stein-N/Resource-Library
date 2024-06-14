a1de0b4ee56bb69add54804f44135a8e739dcd5f
Stein-N <nico-stein-98@outlook.de>
2024-06-14 16:08:27 +0200

    common: minor change

--- common/src/main/java/net/xstopho/resourcelibrary/platform/IPlatformHelper.java
+++ common/src/main/java/net/xstopho/resourcelibrary/platform/IPlatformHelper.java
@@ -4,9 +4,9 @@ public interface IPlatformHelper {
 
     default Platforms getPlatform() {
         return Platforms.NO_LOADER;
-    };
+    }
 
     enum Platforms {
-        FORGE, FABRIC, NEOFORGE, NO_LOADER;
+        FORGE, FABRIC, NEOFORGE, NO_LOADER
     }
 }
