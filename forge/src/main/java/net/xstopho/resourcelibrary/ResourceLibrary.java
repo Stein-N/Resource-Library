cbdd4e2cad868d0f7e1a0f3d02a4d61fca736c6e
Stein-N <nico-stein-98@outlook.de>
2024-06-14 16:06:17 +0200

    project: bump versions to 1.21 and added forge back

---
+++ forge/src/main/java/net/xstopho/resourcelibrary/ResourceLibrary.java
@@ -0,0 +1,11 @@
+package net.xstopho.resourcelibrary;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(LibConstants.MOD_ID)
+public class ResourceLibrary {
+
+    public ResourceLibrary() {
+
+    }
+}
---
+++ forge/src/main/java/net/xstopho/resourcelibrary/datagen/ResourceBlockStateProvider.java
@@ -0,0 +1,28 @@
+package net.xstopho.resourcelibrary.datagen;
+
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+
+public abstract class ResourceBlockStateProvider extends BlockStateProvider {
+
+    public ResourceBlockStateProvider(PackOutput output, String modid, ExistingFileHelper exFileHelper) {
+        super(output, modid, exFileHelper);
+    }
+
+    public void createFurnaceLikeBlock(Block block) {
+        horizontalBlock(block, modifyBlockKey(block, "_side"), modifyBlockKey(block, ""), modifyBlockKey(block, "_top"));
+    }
+
+    private ResourceLocation modifyBlockKey(Block block, String texturePosition) {
+        String[] parts = getBlockKey(block).toString().split(":");
+        return ResourceLocation.fromNamespaceAndPath(parts[0], "block/" + parts[1] + texturePosition);
+    }
+
+    private ResourceLocation getBlockKey(Block block) {
+        return BuiltInRegistries.BLOCK.getKey(block);
+    }
+}
