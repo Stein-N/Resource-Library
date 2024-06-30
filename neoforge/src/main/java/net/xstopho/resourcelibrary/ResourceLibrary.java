a122114ee12d898869e06ca191db693d2c163175
Stein-N <nico-stein-98@outlook.de>
2024-06-30 15:21:27 +0200

    neoforge: test modules get initialized when in Dev environment

--- neoforge/src/main/java/net/xstopho/resourcelibrary/ResourceLibrary.java
+++ neoforge/src/main/java/net/xstopho/resourcelibrary/ResourceLibrary.java
@@ -1,12 +1,15 @@
 package net.xstopho.resourcelibrary;
 
-import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.loading.FMLLoader;
+import net.xstopho.resourcelibrary.test.LibraryTest;
 
 @Mod(LibConstants.MOD_ID)
 public class ResourceLibrary {
 
     public ResourceLibrary() {
-
+        if (!FMLLoader.isProduction()) {
+            LibraryTest.init();
+        }
     }
 }
